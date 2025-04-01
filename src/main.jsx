6262303e9d4e20f4bf6b0f9579ee553ddfcb41f9
horangifence <6733387@naver.com>
2025-04-02 02:26:04 +0900

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>호랭이 휀스</h1>
+    <App />
   </React.StrictMode>
 );
