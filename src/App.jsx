32b390369eb62634e8f490e8263c2efda5921c1e
horangifence <6733387@naver.com>
2025-04-02 01:37:37 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Estimate from "./pages/Estimate";
+import Blog from "./pages/Blog"; // ✅ 블로그 페이지 import 추가
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/estimate" element={<Estimate />} />
+        <Route path="/blog" element={<Blog />} /> {/* ✅ 블로그 라우터 추가 */}
       </Routes>
     </Router>
   );
